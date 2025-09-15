7f406c93be38c5cc1df095606cc1558819a2e947
Adam Thomas <thomasadam911@yahoo.com>
2025-09-15 15:33:21 -0500

    Adding part 6

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,23 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works! {{housingLocationId}}
-    </p>
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo" [alt]="housingLocation?.name">
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Does this location have Wifi: {{ housingLocation?.wifi ? 'Yes' : 'No' }}</li>
+          <li>Does this location have laundry: {{ housingLocation?.laundry ? 'Yes' : 'No' }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to stay here</h2>
+        <button class="primary">Apply</button>
+      </section>
+    </article>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+    const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 }

d8dcb826a3d765757ae58f8ddc6fcc6b53878ad5
Adam Thomas <thomasadam911@yahoo.com>
2025-09-15 16:46:04 -0500

    Added part 8

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
 import { FormControl,FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { first } from 'rxjs';
+
 
 @Component({
   selector: 'app-details',
@@ -12,19 +12,22 @@ import { first } from 'rxjs';
   imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
+
       <img class="listing-photo" [src]="housingLocation?.photo" [alt]="housingLocation?.name">
       <section class="listing-description">
         <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
         <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
       </section>
+
       <section class="listing-features">
         <h2 class="section-heading">About this location</h2>
         <ul>
           <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have Wifi: {{ housingLocation?.wifi ? 'Yes' : 'No' }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry ? 'Yes' : 'No' }}</li>
+          <li>Does this location have Wifi: {{ housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{ housingLocation?.laundry}}</li>
         </ul>
       </section>
+
       <section class="listing-apply">
         <h2 class="section-heading">Apply now to stay here</h2>
         <form [formGroup]="applyForm" (submit)="submitApplication()">
@@ -36,7 +39,7 @@ import { first } from 'rxjs';
 
           <label for="email">Email</label>
           <input id="email" type="text" formControlName="email">
-          <button type="submit" class="primary">Submit</button>
+          <button type="submit" class="primary">Apply now</button>
         </form>
       </section>
     </article>
@@ -55,7 +58,9 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   submitApplication() {

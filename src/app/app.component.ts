4069eadd87c91b2b80807dadc2e253f99dd5e011
Adam Thomas <thomasadam911@yahoo.com>
2025-10-03 04:24:30 -0500

    Removed unused home referance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HomeComponent, RouterModule],
+  imports: [RouterModule],
   template: `
     <main>
       <header class="brand-name">

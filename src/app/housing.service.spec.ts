a878e43f8584971b223853f026b6e1b164f190b2
Adam Thomas <thomasadam911@yahoo.com>
2025-10-05 12:24:04 -0500

    Added additonal testing

--- src/app/housing.service.spec.ts
+++ src/app/housing.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HousingService } from './housing.service';
+import type { HousingLocation } from './housing-location';
 
 describe('HousingService', () => {
   let service: HousingService;
@@ -12,5 +13,20 @@ describe('HousingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Returns json data from getAllHousingLocations', async () => {
+    const locations = await service.getAllHousingLocations();
+    expect(locations.length).toBeGreaterThan(0);
+    expect(locations[0].name).toBeDefined();
+  });
+
+  it('Returns json data from getHousingLocationById', async () => {
+    const location : HousingLocation|undefined = await service.getHousingLocationById(1);
+    expect(location).toBeDefined();
+    expect(location?.id).toBeDefined();
+    console.log(typeof location?.id)
+    const val = parseInt(String(location?.id ?? '0'));
+    expect(val).toBe(1);
   });
 });
